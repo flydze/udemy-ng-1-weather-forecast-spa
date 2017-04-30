bf1de706739991fa769d6764614af3281b7d897e
Valentyn Zozulia <flydze@gmail.com>
2017-04-29 22:35:57 -0300

    Implement displaying forecast for different count of days

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ weatherApp.config(function ($routeProvider) {
     .when('/forecast', {
       templateUrl: 'views/forecast.htm',
       controller : 'forecastController'
+    })
+    .when('/forecast/:days', {
+      templateUrl: 'views/forecast.htm',
+      controller : 'forecastController'
     });
 });
 
@@ -23,6 +27,19 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
   });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
   $scope.city = cityService.city;
+  $scope.days = $routeParams.days || 2;
+
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=84a29bb569b6dca74aa27273aa6d55ad&q="+$scope.city+"&cnt="+$scope.days);
+
+  $scope.weatherResult = $scope.weatherAPI.get();
+
+  $scope.convertToCelcius = function (degK) {
+    return Math.round(degK - 273);
+  };
+
+  $scope.convertToDate = function (dt) {
+    return new Date(dt * 1000);
+  }
 }]);

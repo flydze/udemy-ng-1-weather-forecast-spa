379177a082b588fbe03f04e3d7c3f280166e4b6a
Valentyn Zozulia <flydze@gmail.com>
2017-05-01 13:54:57 -0300

    Add possibility to select count of days to show

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
   $scope.city = cityService.city;
-  $scope.days = $routeParams.days || 2;
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=84a29bb569b6dca74aa27273aa6d55ad&q="+$scope.city+"&cnt="+$scope.days);
 
